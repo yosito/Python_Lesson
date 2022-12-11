1972fe98d9c465178d8a8b19cf83a76b2f18625e
yosito <84324917+yosito@users.noreply.github.com>
2022-12-11 16:38:35 +0900

    Add files via upload

---
+++ Python_Lesson/bmi.py
@@ -0,0 +1,4 @@
+h = float(input("身長何cmですか？")) / 100
+w = float(input("体重何kgですか？"))
+bmi = w / (h * h)
+print("あなたのBMIは、",bmi,"です。" )
---
+++ Python_Lesson/omikuji.py
@@ -0,0 +1,3 @@
+import random
+kujo =["大吉","中吉","小吉","凶"]
+print(random.choice(kujo))
